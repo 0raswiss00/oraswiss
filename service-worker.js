136eae2647c7a7736dff80bda5ecb590d2bf3c6d
0raswiss00 <adicela899@gmail.com>
2026-01-04 16:49:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,33 @@
+const CACHE = "oraswiss-v2";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/profile.html",
+  "/styles.css"
+];
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("ora-swiss-v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/profile.html",
-        "/styles.css"
-      ]);
-    })
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
+
 

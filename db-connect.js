31fefaa60340378817a0b3f1044d8a8d6c07b0e4
0raswiss00 <adicela899@gmail.com>
2026-07-05 22:20:26 +0200

    Update db-connect.js

--- db-connect.js
+++ db-connect.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://gnwsrmelmeqduhhmfmoa.supabase.co';
-const supabaseKey = 'PASTE_YOUR_ANON_KEY_HERE';
+const supabaseKey = 'sb_publishable__AkK_Z7lhtkvukN6Fy1cAQ_KBTXIcY9';
 
 // krijo klientin vetëm një herë
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);

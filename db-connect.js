adde15d153e85e3d208d32b5597fd1bd1ed01b75
0raswiss00 <adicela899@gmail.com>
2026-07-05 22:02:00 +0200

    Update db-connect.js

--- db-connect.js
+++ db-connect.js
@@ -5,25 +5,6 @@ const supabaseKey = 'sb_publishable__AkK_Z7lhtkvukN6Fy1cAQ_KBTXIcY9';
 // Krijimi i klientit
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-// Funksioni kryesor për të marrë të dhënat
-async function testo() {
-    console.log("Po lidhemi me bazën 'oras'...");
-    
-    try {
-        const { data, error } = await supabase.from('oras').select('*');
-        
-        if (error) {
-            console.error("Gabim nga Supabase:", error.message);
-        } else {
-            console.log("Lidhja sukses! Ja të dhënat:", data);
-        }
-    } catch (err) {
-        console.error("Gabim gjatë ekzekutimit:", err);
-    }
-}
-
-// Thirrja e funksionit
-testo();
 // Testim lidhjeje
 async function testoLidhjen() {
     const { data, error } = await supabase.from('oras').select('*');

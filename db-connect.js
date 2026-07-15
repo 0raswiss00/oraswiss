375c00d086548abb940d8b24f3f181140b5ee284
0raswiss00 <adicela899@gmail.com>
2026-07-15 22:33:48 +0200

    Update db-connect.js

--- db-connect.js
+++ db-connect.js
@@ -2,7 +2,7 @@ const supabaseUrl = "https://gnwsrmelmeqduhhmfmoa.supabase.co";
 
 const supabaseKey = "sb_publishable__AkK_Z7lhtkvukN6Fy1cAQ_KBTXIcY9";
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
